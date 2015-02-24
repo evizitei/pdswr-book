8593f1c9db5d36b0d596ceaf6b349b4f6c3d0ede
Ethan Vizitei <ethan.vizitei@gmail.com>
2015-02-24 09:21:36 -0700

    log csv vs URL loading, read.table can be used for both

---
+++ car_data_loading.r
@@ -0,0 +1,8 @@
+# this totally works, but takes a while on slow connections, use the file loader in general
+# carData <- read.table(
+#   'http://www.win-vector.com/dfiles/car.data.csv',
+#   sep=',', header=T
+# )
+
+# comment this out if using the online loader^
+carData <- read.csv("~/Code/data-analysis/practical-data-science-with-R/data/car.data.csv")
